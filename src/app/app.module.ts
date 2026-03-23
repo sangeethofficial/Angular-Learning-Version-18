fb78329f28f87dadcc1088c24ef4d9d2855848dc
sangeethkumar.r <sangeethkumar.r@KTP-RLTT-KIT030>
2026-03-23 10:26:11 +0530

    updated the main branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LearnComponent } from './components/learn/learn.component';
 import { LearnProjectComponent } from './components/learn-project/learn-project.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LearnProjectComponent } from './components/learn-project/learn-project.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/learn/learn.component.ts
+++ src/app/components/learn/learn.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-learn',
@@ -6,6 +6,31 @@ import { Component } from '@angular/core';
   // template:`<app-learn-project></app-learn-project>`,
   styleUrl: './learn.component.css'
 })
-export class LearnComponent {
+export class LearnComponent implements OnInit{
 
+  userId:number = 10;
+  userStatus:string='Offline';
+  allowNewUser:boolean = false;
+  userCreatedStatus='no user is created';
+  userName = 'sangeeth';
+  isUserCreated = false;
+
+  constructor(){
+    setTimeout(()=>{ 
+      this.allowNewUser = true;
+    },3000);
+  }
+
+  ngOnInit(): void {
+  }
+  getUserStatus(){
+   return this.userStatus;
+  }
+  submitUser(){
+    this.isUserCreated = true;
+    this.userCreatedStatus = 'user is created ';
+  }
+  onUpdateUser(event:Event){
+    this.userName = (event.target as HTMLInputElement).value;
+  }
 }
